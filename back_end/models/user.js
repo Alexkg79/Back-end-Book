36c06e5929aece0af3b74c7fd4a42c5e7cf32ad7
Alex Marolleau <Alex.M79@outlook.com>
2024-08-10 13:47:47 +0200

    MAJ de model user

--- back_end/models/user.js
+++ back_end/models/user.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/],
+  },
+  password: { type: String, required: true, minlength: 6 },
 });
 
 userSchema.pre('save', async function hashPassword(next) {
