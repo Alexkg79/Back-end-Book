621fb2a20bfa0633a479577715c2a2b3d52ec197
alx <unixthegamer@gmail.com>
2025-07-07 14:59:33 +0200

    Finish Render

--- back_end/server.js
+++ back_end/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
+
 const authRoutes = require('./routes/auth');
 const bookRoutes = require('./routes/books');
 const authMiddleware = require('./middleware/auth');
@@ -12,26 +13,44 @@ dotenv.config({ path: './.env' });
 console.log('MONGO_URI:', process.env.MONGO_URI);
 
 const app = express();
+
 app.use(express.json());
+
+const allowedOrigins = [
+  'http://localhost:3000',      // Pour le développement local
+  process.env.FRONTEND_URL       // Pour le site en production sur Render
+];
+
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'La politique CORS pour ce site n\'autorise pas l\'accès depuis cette origine.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
 }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// --- Connexion à la base de données MongoDB ---
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((error) => {
-    console.log('Connexion à MongoDB échouée !', error);
+    console.error('Connexion à MongoDB échouée !', error);
     process.exit(1);
   });
 
+// --- Définition des routes de l'API ---
 app.use('/api/auth', authRoutes);
 app.use('/api/', bookRoutes);
 
+// Exemple de route protégée par authentification
 app.get('/api/protected-route', authMiddleware, (req, res) => {
-  res.status(200).json({ message: 'Accès autorisé!' });
+  res.status(200).json({ message: 'Accès autorisé !' });
 });
 
 const PORT = process.env.PORT || 4000;
